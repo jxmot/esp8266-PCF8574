3460c789fa4caa71fedfd6ed5c0ba4d62addd3c5
jxmot <webdev@stupiddog.net>
2018-03-15 15:38:03 -0500

    refactored, see pcf8574.h/cpp

--- esp8266-PCF8574.ino
+++ esp8266-PCF8574.ino
@@ -15,6 +15,7 @@
 // is for reading switches and the other one toggles LEDs
 pcf8574 *p_pcf8574_rd = NULL;
 pcf8574 *p_pcf8574_wr = NULL;
+pcf8574 *p_pcf8574 = NULL;
 
 // for interrupts that occur when an input changes
 volatile bool intrFlag;
@@ -24,6 +25,7 @@ void ICACHE_RAM_ATTR intrHandler()
 }
 
 /*
+    Set up the application....
 */
 void setup()
 {
@@ -34,8 +36,16 @@ void setup()
     intrFlag = true;
 
     // Change the GPIOx labels to suit your ESP8266 platform
-    p_pcf8574_rd = new pcf8574("GPIO0", "GPIO2", 0x20, intrHandler);
-    p_pcf8574_wr = new pcf8574("GPIO0", "GPIO2", 0x21);
+    p_pcf8574 = new pcf8574("GPIO0", "GPIO2");
+    // NOTE : Regarding interrupts it is only necessary to pass the 
+    // address to the interrupt handler one time. It is up to the
+    // client to decide which devices to read when an interrupt occurs.
+    // However it is probably best to read all inputs on all devices 
+    // that are attached to the I2C bus when an interrupt happens.
+    p_pcf8574->createDevice(0x20, intrHandler);
+    // typically when creating a device without an interrupt handler
+    // it would mean that this device is dedicated for output
+    p_pcf8574->createDevice(0x21);
 
     while (!Serial);
     Serial.println();
@@ -46,19 +56,20 @@ void setup()
 
 //////////////////////////////////////////////////////////////////////////////
 /*
+    Ever feel like your running in circles?
 */
 void loop()
 {
 static int modcount = 0;
 
     // alternate between write tests...
-    if(((modcount += 1) % 30) > 15) testCount(p_pcf8574_wr);
-    else testShift(p_pcf8574_wr);
+    if(((modcount += 1) % 30) > 15) testCount(0x21, p_pcf8574);
+    else testShift(0x21, p_pcf8574);
     Serial.flush();
 
     // the test will manage the state of the
     // interrupt flag
-    intrFlag = testRead(p_pcf8574_rd, intrFlag);
+    intrFlag = testRead(0x20, p_pcf8574, intrFlag);
     Serial.flush();
 
     delay(250);
